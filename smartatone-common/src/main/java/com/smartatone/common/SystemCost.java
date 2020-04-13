93c38376dc1150bd79b0229599b8f26e799b8525
startatone <javawebxy@163.com>
2020-04-13 13:29:36 +0800

    init

---
+++ smartatone-common/src/main/java/com/smartatone/common/SystemCost.java
@@ -0,0 +1,10 @@
+package com.smartatone.common;
+
+/**
+ * @author xuyang001
+ * @Description: TODO
+ * @create 2020-04-13 13:12
+ **/
+public class SystemCost {
+    public static String a = "a";
+}
